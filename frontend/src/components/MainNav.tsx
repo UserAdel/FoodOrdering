2337e1eb608a18cd769f5c080fe6e92d7c572f09
adel hany <adel58420@gmail.com>
2025-06-18 04:48:41 +0300

    UsernameMenu&MobileNavbar

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Button } from './ui/button'
+import { useAuth0 } from '@auth0/auth0-react'
+import UsernameMenu from './UsernameMenu';
 
 export default function  () {
-   return (
+  const {loginWithRedirect,isAuthenticated}=useAuth0();
 
-    <Button variant='ghost' className='font-bold text-md hover:text-orange-500 hover:bg-white'>
+
+   return (
+    <span className="flex space-x-2 items-center">
+      {isAuthenticated ? (<UsernameMenu/>):(  <Button onClick={async()=> await loginWithRedirect() } variant='ghost' className='font-bold text-lg hover:text-orange-500 hover:bg-white'>
         Log In
-    </Button>
+    </Button>)} 
+    </span>
+  
 
 
   )
