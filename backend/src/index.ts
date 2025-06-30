97fb61b7c3470240519e65228d2f4ee75b0bbd96
adel hany <adel58420@gmail.com>
2025-06-30 05:18:46 +0300

    cors fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ import orderRoute from "./routes/OrderRoute";
 const app = express();
 app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
